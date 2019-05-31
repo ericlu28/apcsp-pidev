e80e11331941de022459bad520de32b710b50bba
Eric Lu <eric.andrew.lu@gmail.com>
2019-05-31 19:37:50 +0000

    cdemo

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,23 +1,64 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <math.h>
 
-float areaofcircle(float radius)
+
+float myAreaFunction(float radius)
 {
-  float area;
-  area= M_PI*radius*radius;
-  return area;
+	float area = M_PI * radius * radius;
+	return area;
 }
 
-int main(){
-  char input[256];
-  float r;
-  printf("What is your circle's radius? \n");
-  while (1)
-  {
-    fgets(input, 256, stdin);
-    if (sscanf(input, "%f", &r) ==1) break;
-    printf("Please enter a valid radius: \n");
-  }
-  float area= areaofcircle(r);
-  printf("The area of your circle is %f\n", area);
+
+int main(int argc, char* argv[])
+{
+	if (argc == 3)
+	{
+	        int arg1;
+       		int found = sscanf(argv[1], "%d", &arg1);
+       		if (found != 1)
+       		{
+               		printf("first arg is not an integer, enter two ints\n");
+               		return 1;
+       		}
+
+	        int arg2;
+       		found = sscanf(argv[2], "%d", &arg2);
+       		if  (found != 1)
+       		{
+               		printf("second arg is not an integer, enter two ints\n");
+                	return 1;
+		}
+		for (int i = arg1; i <= arg2; i++)
+                {
+                        float ans = myAreaFunction(i);
+                        printf("%f\n", ans);
+                }
+	}
+
+	else
+	{
+		char input[256];
+		float lower;
+		float upper;
+		printf("Enter the lower bound of radii of which you want to find the area of a circle.\n");
+		while (1)
+		{
+			fgets(input, 256, stdin);
+			if (sscanf(input, "%f", &lower) == 1) break;
+			printf("Not a valid number. Try again!\n");
+		}
+		printf("Enter the upper bound of radii of which you want to find the area of a circle.\n");
+		while (1)
+		{
+			fgets(input, 256, stdin);
+			if (sscanf(input, "%f", &upper) == 1) break;
+			printf("Not a valid number. Try again!\n");
+		}
+		for (int i = lower; i <= upper; i++)
+       		{
+       	        	float ans = myAreaFunction(i);
+       	        	printf("%f\n", ans);
+       		}
+
+	}
 }
